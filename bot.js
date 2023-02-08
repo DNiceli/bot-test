3bc6962b017bdcc2441ea68996b2dd2ed3a781ba
deniz <denizarslan94@googlemail.com>
2023-02-08 16:25:03 +0100

    basic stuff vom guide

--- bot.js
+++ bot.js
@@ -1,24 +1,46 @@
 require('dotenv').config()
+const fs = require('node:fs');
+const path = require('node:path');
 
-const { Client, Events, GatewayIntentBits, InteractionCollector, messageLink } = require('discord.js');
-const client = new Client({ 
-    intents: [
-        GatewayIntentBits.Guilds,
-        GatewayIntentBits.GuildMessages,
-        GatewayIntentBits.MessageContent,
-        GatewayIntentBits.GuildMembers,
-], 
-});
+
+const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+
+client.commands = new Collection();
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const filePath = path.join(commandsPath, file);
+	const command = require(filePath);
+    if ('data' in command && 'execute' in command) {
+		client.commands.set(command.data.name, command);
+	} else {
+		console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+	}
+}
 
 client.on('ready', () => {
     console.log("Discord Bot is logged in!")
 });
 
-client.on('messageCreate', (msg) => {
-    if (msg.content === "is mee") {
-        msg.reply("denmips uabbeth")
-        msg.react("❤️")
-    }
+client.on(Events.InteractionCreate, async interaction => {
+	if (!interaction.isChatInputCommand()) return;
+
+	const command = client.commands.get(interaction.commandName);
+
+	if (!command) {
+		console.error(`No command matching ${interaction.commandName} was found.`);
+		return;
+	}
+
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+	}
 });
 
 client.login(process.env.BOT_TOKEN)
