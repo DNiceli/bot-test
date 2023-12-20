9a4295713b79821c3bf7ea62f8aa1557a08bf3fd
dnice <denizarslan94@googlemail.com>
2023-12-20 19:11:39 +0100

    modified Speiseplan2 so that it works with saved Data and doesnt fetch new Data, also added images to the cards

--- commands/speiseplan2.js
+++ commands/speiseplan2.js
@@ -1,11 +1,10 @@
 require("dotenv").config();
-const axios = require("axios");
 const sharp = require("sharp");
-const cheerio = require("cheerio");
 const puppeteer = require("puppeteer");
 const { SlashCommandBuilder } = require("discord.js");
 const Dish = require("../models/Dish.js");
 const Favorite = require("../models/Favorite.js");
+const Menu = require("../models/Dailymenu.js");
 
 const arrowLeft = "\u2B05";
 const arrowRight = "\u27A1";
@@ -21,6 +20,16 @@ const categoryEmojis = {
   Beilagen: "\uD83C\uDF5A",
 };
 
+let menuImgs = {
+  Vorspeisen: [],
+  Salate: [],
+  Essen: [],
+  Desserts: [],
+  Suppen: [],
+  Aktionen: [],
+  Beilagen: [],
+};
+
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("speiseplan2")
@@ -28,128 +37,84 @@ module.exports = {
   async execute(interaction) {
     try {
       await interaction.deferReply();
-      const url = process.env.mensaUrl2;
-      const menu = new Map();
-      await axios
-        .post(
-          url,
-          new URLSearchParams({
-            resources_id: "527", // bht ID: 527
-            date: "2023-10-04", // Datum
-          }),
-          {
-            headers: {
-              "x-requested-with": "XMLHttpRequest",
-            },
-          }
-        )
-        .then((response) => {
-          const html = response.data;
-          const $ = cheerio.load(html);
+      const today = new Date().toISOString().split("T")[0];
+      let dailyMenu = await Menu.findOne({ date: today });
+      if (!dailyMenu) {
+        await interaction.editReply("No menu found for today");
+      } else {
+        let imgs = [];
+        dailyMenu = await dailyMenu.populate("dishes");
 
-          $(".container-fluid.splGroupWrapper").each((_, groupWrapper) => {
-            const group = $(groupWrapper).find(".splGroup").text().trim();
-            const dishes = [];
-
-            $(groupWrapper)
-              .find(".row.splMeal")
-              .each(async (_, meal) => {
-                const dish = {
-                  name: $(meal)
-                    .find(".col-xs-6.col-md-5 > .bold")
-                    .text()
-                    .trim(),
-                  price: $(meal)
-                    .find(".col-xs-12.col-md-3.text-right")
-                    .text()
-                    .trim(),
-                  allergens: $(meal).attr("lang"),
-                  ampelpunkt: $(meal).find("img.splIcon").attr("alt"),
-                  h2p: $(meal)
-                    .find("img[aria-describedby^='tooltip_H2O']")
-                    .attr("alt"),
-                  co2: $(meal)
-                    .find("img[aria-describedby^='tooltip_CO2']")
-                    .attr("alt"),
-                };
-                console.log(dish.allergens);
-                dishes.push(dish);
-
-                Dish;
-              });
-
-            menu.set(group, dishes);
-          });
-          //console.log(menu);
-        })
-        .catch((error) => {
-          console.log(error);
-        });
-
-      const deserts = menu.get("Desserts");
-      const img = await generateMenuCard(deserts[0]);
-      const message = await interaction.editReply({
-        content: "Here's the menu card:",
-        files: [img],
-      });
-
-      for (const category of menu.keys()) {
-        console.log(category);
-        await message.react(categoryEmojis[category]);
-      }
-
-      await message.react(arrowLeft);
-      await message.react(arrowRight);
-      await message.react(star);
-
-      // Create the message collector to listen for reactions of users
-      const filter = (reaction, user) => user.id === interaction.user.id;
-      const collector = message.createReactionCollector({
-        filter,
-        time: 60000,
-      });
-
-      let currentCategory = "Desserts";
-      let currentIndex = 0;
-
-      collector.on("collect", async (reaction, user) => {
-        const reactedEmoji = reaction.emoji.name;
-        const selectedCategory = Object.keys(categoryEmojis).find(
-          (category) => categoryEmojis[category] === reactedEmoji
-        );
-
-        if (selectedCategory) {
-          currentCategory = selectedCategory;
-          currentIndex = 0;
-        } else if (reactedEmoji === arrowLeft) {
-          currentIndex =
-            (currentIndex - 1 + menu.get(currentCategory).length) %
-            menu.get(currentCategory).length;
-        } else if (reactedEmoji === arrowRight) {
-          currentIndex = (currentIndex + 1) % menu.get(currentCategory).length;
-        } else if (reactedEmoji === star) {
-          if (!interaction.guild) return; // Returns as there is no guild
-          var guild = interaction.guild.id;
-          var userID = interaction.user.id;
-          var dishId = menu.get(currentCategory)[currentIndex].name;
-          Favorite.createOrUpdateFavorite(guild, userID, dishId);
+        for (const dish of dailyMenu.dishes) {
+          let dishImgObj = await generateMenuCard(dish);
+          menuImgs[dishImgObj.category].push(dishImgObj);
         }
 
-        const dish = menu.get(currentCategory)[currentIndex];
-
-        const newImg = await generateMenuCard(dish, currentCategory);
-        await message.edit({
+        let img = menuImgs["Desserts"][0].image;
+        const message = await interaction.editReply({
           content: "Here's the menu card:",
-          files: [newImg],
+          files: [img],
         });
 
-        await reaction.users.remove(interaction.user.id);
-      });
+        for (const category of Object.keys(menuImgs)) {
+          console.log(category);
+          await message.react(categoryEmojis[category]);
+        }
 
-      collector.on("end", () => {
-        // Remove reactions when the collector ends
-        message.reactions.removeAll();
-      });
+        await message.react(arrowLeft);
+        await message.react(arrowRight);
+        await message.react(star);
+
+        // Create the message collector to listen for reactions of users
+        const filter = (reaction, user) => user.id === interaction.user.id;
+        const collector = message.createReactionCollector({
+          filter,
+          time: 60000,
+        });
+
+        let currentCategory = "Desserts";
+        let currentIndex = 0;
+
+        collector.on("collect", async (reaction, user) => {
+          const reactedEmoji = reaction.emoji.name;
+          const selectedCategory = Object.keys(categoryEmojis).find(
+            (category) => categoryEmojis[category] === reactedEmoji
+          );
+
+          if (selectedCategory) {
+            currentCategory = selectedCategory;
+            currentIndex = 0;
+          } else if (reactedEmoji === arrowLeft) {
+            currentIndex =
+              (currentIndex - 1 + menuImgs[currentCategory].length) %
+              menuImgs[currentCategory].length;
+          } else if (reactedEmoji === arrowRight) {
+            currentIndex =
+              (currentIndex + 1) % menuImgs[currentCategory].length;
+          } else if (reactedEmoji === star) {
+            if (!interaction.guild) return; // Returns as there is no guild
+            var guild = interaction.guild.id;
+            var userID = interaction.user.id;
+            var dishId = menuImgs[currentCategory][currentIndex]._id;
+            Favorite.createOrUpdateFavorite(guild, userID, dishId);
+          }
+
+          const dish = menuImgs[currentCategory][currentIndex];
+
+          const newImg = dish.image;
+          await message.edit({
+            content: "Here's the menu card:",
+            files: [newImg],
+          });
+
+          await reaction.users.remove(interaction.user.id);
+        });
+
+        collector.on("end", () => {
+          // Remove reactions when the collector ends
+          message.reactions.removeAll();
+        });
+      }
     } catch (error) {
       console.error(error);
       await interaction.editReply("ERROR ERROR, siehe console");
@@ -176,7 +141,7 @@ async function generateMenuCard2(dish) {
   return pngBuffer;
 }
 
-async function generateMenuCard(dish, currentCategory) {
+async function generateMenuCard(dish) {
   const categoryColors = {
     Vorspeisen: "#FFDAB9",
     Essen: "#ADD8E6",
@@ -187,7 +152,10 @@ async function generateMenuCard(dish, currentCategory) {
     Aktionen: "#E6E6FA",
   };
 
-  const color = categoryColors[currentCategory] || "#FFFFFF"; // default to white if category is unknown
+  const color = categoryColors[dish.category] || "#FFFFFF"; // default to white if category is unknown
+
+  let dishImage = await dish.populate("imageId");
+  let url = dishImage.imageId.url;
 
   const htmlTemplate = `
   <!DOCTYPE html>
@@ -239,7 +207,8 @@ async function generateMenuCard(dish, currentCategory) {
       </style>
     </head>
     <body>
-      <div class="image-placeholder">IMG</div>
+    <div class="image-placeholder">
+    <img src="${url}" alt="Dish Image" style="width:100%; height:100%;"></div>
       <div class="name">${dish.name}</div>
       <div class="price">Price: ${dish.price}</div>
       <div class="allergens">Allergens: ${dish.allergens}</div>
@@ -254,5 +223,11 @@ async function generateMenuCard(dish, currentCategory) {
     clip: { x: 0, y: 0, width: 400, height: 200 },
   });
   await browser.close();
-  return pngBuffer;
+  let returnObject = {
+    id: dish._id,
+    image: pngBuffer,
+    name: dish.name,
+    category: dish.category,
+  };
+  return returnObject;
 }
